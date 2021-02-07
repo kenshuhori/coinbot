216dbbecdb79682e119b808d5f0cd8a5d813035f
chgnr <41231468+chgnr@users.noreply.github.com>
2021-02-07 17:25:07 +0900

    Update ryochin.js (#2)

--- ryochin.js
+++ ryochin.js
@@ -17,14 +17,34 @@ const signature = crypto.createHmac('sha256', secret)
   .update(message)
   .digest('hex')
 
+
 const axios = require('axios')
-axios.get(uri.href, {
-  headers: {
-    "ACCESS-KEY": key,
-    "ACCESS-NONCE": nonce,
-    "ACCESS-SIGNATURE": signature
-  }
-})
+
+// 残高確認
+// axios.get(uri.href, {
+//   headers: {
+//     "ACCESS-KEY": key,
+//     "ACCESS-NONCE": nonce,
+//     "ACCESS-SIGNATURE": signature
+//   }
+// })
+//   .then(res => {
+//     console.log(res)
+//   })
+
+// const getOrderBooksData = async () => {
+//   const ticker = "BTC"
+// }
+
+
+const getData = () => {
+  axios.get(`https://coincheck.com/api/ticker`)
   .then(res => {
-    console.log(res)
+    console.log(res.data.last) // last: 最後の取引の価格
   })
+}
+
+// 1 秒おきに実行
+setInterval(() => {
+  getData()
+}, 1000);
