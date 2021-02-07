3b65299f1c21259fa64569de01fb2f217995b723
KenshuHori <kenshuhori@gmail.com>
2021-02-07 17:25:40 +0900

    2秒間隔で確認する

--- kenshu.js
+++ kenshu.js
@@ -20,7 +20,7 @@ var params = {
 /** Public API */
 coinCheck.ticker.all(params);
 // coinCheck.trade.all(params);
-coinCheck.orderBook.all(params);
+// coinCheck.orderBook.all(params);
 
 /** Private API */
 
@@ -29,16 +29,14 @@ coinCheck.orderBook.all(params);
 // "sell" 指値注文 現物取引 売り
 // "market_buy" 成行注文 現物取引 買い
 // "market_sell" 成行注文 現物取引 売り
-// "leverage_buy" 指値注文 レバレッジ取引新規 買い
-// "leverage_sell" 指値注文 レバレッジ取引新規 売り
-// "close_long" 指値注文 レバレッジ取引決済 売り
-// "close_short" 指値注文 レバレッジ取引決済 買い
-params['data'] = {
-    rate: 2850,
-    amount: 0.00508771,
-    order_type: 'buy',
-    pair: 'btc_jpy'
+// params['data'] = {
+//   rate: 2850,
+//   amount: 0.00508771,
+//   order_type: 'buy',
+//   pair: 'btc_jpy'
+// }
+function getTicker() {
+  coinCheck.ticker.all(params);
 }
 
-// 未決済の注文一覧
-coinCheck.order.opens(params);
+setInterval(getTicker, 2000);
